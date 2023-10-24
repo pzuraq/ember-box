d8844916f33acdb4dfc7aaf10b1f0c1d6129fea6
Boris Petrov <boris_petrov@live.com>
2023-10-24 13:13:39 +0300

    Remove the AST transformation altogether

--- index.js
+++ index.js
@@ -2,14 +2,4 @@
 
 module.exports = {
   name: require('./package').name,
-
-  setupPreprocessorRegistry(type, registry) {
-    registry.add('htmlbars-ast-plugin', {
-      name: 'box-helper',
-      plugin: require('./lib/box-transform'),
-      baseDir() {
-        return __dirname;
-      },
-    });
-  },
 };
